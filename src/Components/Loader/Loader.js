042f5b9aaeb9bddebe1989fb38f8f65e17c34ddc
quentinM22 <quentin.meunier1@laposte.net>
2023-05-17 14:01:48 +0200

    Feat: loader

---
+++ src/Components/Loader/Loader.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Loader.css"
+const Loader = () => {
+	return (
+		<>
+			<div className="cmp-loader">
+				<span className="loader"></span>
+				<h2>Chargement ... </h2>
+			</div>
+		</>
+	)
+}
+
+export default Loader
